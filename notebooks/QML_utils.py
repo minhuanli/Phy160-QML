ee2548a060791204b76dc5803fc10296859514b2
minhuanli <minhuanli@g.harvard.edu>
2022-05-04 21:51:32 -0400

    Combine the CU function from Hal

--- notebooks/QML_utils.py
+++ notebooks/QML_utils.py
@@ -1,6 +1,8 @@
 from qiskit.circuit import QuantumCircuit, ParameterVector
 from qiskit.circuit.library import NLocal
 
+__all__ = ['UnitaryNlocal4', 'UnitaryNlocal2']
+
 def UnitaryNlocal4(reps=3, name='U4'):
     '''
     A utility function to create a parameterized Nlocal circuit 
@@ -75,3 +77,35 @@ def UnitaryNlocal2(reps=2, name='U2'):
                        entanglement_blocks=ent, entanglement='linear',
                        skip_final_rotation_layer=True, insert_barriers=True)
     return qc_nlocal
+
+def Controlled_Unitary(k):
+    '''
+    A utility function to create a controlled unitary with 2 control qubits, 2 target qubits
+    
+    Parameters
+    ----------
+    k:  int tuple, represents control qubit state 
+        (k_1,k_2) are binary, 1 if x is applied 
+
+    Return
+    ------
+    Circuit Object
+    ''' 
+    k_1,k_2 = k
+    qc = QuantumCircuit(4)
+
+    # get qc_nlocal circuit but for 2 qubits 
+    circ = UnitaryNlocal2()
+
+    gate = circ.to_gate
+    if k_1 == 1:
+        qc.x(0)
+    if k_2 == 1:
+        qc.x(1)
+    qc.append(gate.control(2), [0,1,2,3])
+    if k_1 == 1:
+        qc.x(0)
+    if k_2 == 1: 
+        qc.x(1)
+        
+    return qc.to_gate()
